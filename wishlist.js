069d7d5b240c357f6a1badb9aa336480c0de3302
IrinaV1 <122563375+IrinaV1@users.noreply.github.com>
2026-05-16 20:52:27 +0000

    Deploying to gh-pages from @ IrinaV1/shop-app@027370166e647a1f43a2cd5855d5a585dc9d1a2e 🚀

--- wishlist.js
+++ wishlist.js
@@ -1,2 +1,2 @@
-import{g as t}from"./assets/refs-Bbtwc9UY.js";import{f as r,m as a}from"./assets/render-function-CYc9ASim.js";import"./assets/vendor-B1ZHW564.js";const c=t();async function i(){try{const o=await Promise.all(c.map(s=>r(s)));a(o)}catch(o){console.log(o.message)}}i();
+import{g as r}from"./assets/refs-Bbtwc9UY.js";import{f as a,m as c}from"./assets/render-function-CYc9ASim.js";import"./assets/vendor-B1ZHW564.js";async function i(){const s=r();try{const o=await Promise.all(s.map(t=>a(t)));c(o)}catch(o){console.log(o.message)}}i();
 //# sourceMappingURL=wishlist.js.map
